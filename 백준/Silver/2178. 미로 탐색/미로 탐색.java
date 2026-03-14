2fe2bd00e851c43dfc971909dab1e3dd9f234a61
MinGyeong Kim <ssunn0812@gmail.com>
2026-03-14 20:38:45 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14628 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,65 +1,63 @@
 import java.io.*;
 import java.util.*;
 
-class Node {
-    int x, y;
-
-    Node(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
-
-public class Main {
-    static int N, M;
-    static int[][] graph;
-    static int[][] result;
+class Main {
     static boolean[][] visited;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
+    static int[][] graph;
+    static int[] dx = { -1, 1, 0, 0 };
+    static int[] dy = { 0, 0, -1, 1 };
+    static int N, M;
+  
+    static int bfs(int x, int y) {
+        visited[x][y] = true;
 
-    static void bfs(int i, int j) {
-        visited[i][j] = true;
+        Queue<int[]> queue = new LinkedList<>();
+        // 시작점: (1, 1), cnt = 1
+        queue.offer(new int[] { x, y, 1 });
 
-        Queue<Node> q = new LinkedList<>();
-        q.offer(new Node(i, j));
+        while(!queue.isEmpty()) {
+            int[] cur = queue.poll();
 
-        while(!q.isEmpty()) {
-            Node n = q.poll();
+            for(int i = 0; i < 4; i++) {
+                int tx = cur[0] + dx[i];
+                int ty = cur[1] + dy[i];
+                int tCnt = cur[2]; 
 
-            for(int k = 0; k < 4; k++) {
-                int x = n.x + dx[k];
-                int y = n.y + dy[k];
+                if(tx == N && ty == M) {
+                    return tCnt + 1;
+                }
 
-                if(x >= 0 && x < N && y >= 0 && y < M) {
-                    if(graph[x][y] == 1 && !visited[x][y]) {
-                        result[x][y] = result[n.x][n.y] + 1;
-                        visited[x][y] = true;
-                        q.offer(new Node(x, y));
+                if(tx > 0 && tx <= N && ty > 0 && ty <= M) {
+                    if(!visited[tx][ty] && graph[tx][ty] == 1) {
+                        visited[tx][ty] = true;
+                        queue.offer(new int[] { tx, ty, tCnt + 1 });
                     }
                 }
             }
         }
+
+        return 0;
     }
-    
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
+
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+        
+        graph = new int[N + 1][M + 1];
+        visited = new boolean[N + 1][M + 1];
 
-        graph = new int[N][M];
-        visited = new boolean[N][M];
-        result = new int[N][M];
-
-        for(int i = 0; i < N; i++) {
+        for(int i = 1; i <= N; i++) {
             String s = br.readLine();
-            for(int j = 0; j < M; j++)
-                graph[i][j] = s.charAt(j) - '0';
+            for(int j = 1; j <= M; j++) {
+                graph[i][j] = s.charAt(j - 1) - '0';
+            }
         }
-        bfs(0, 0);
-        System.out.println(result[N - 1][M - 1] + 1);
-    }
+
+        System.out.println(bfs(1, 1));
+
+	}
 }
